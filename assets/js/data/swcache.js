e310537318a66e28291362eee1738edd50dbd029
Gracee <graceewilson090205@gmail.com>
2022-09-22 22:00:35 +0800

    static

---
+++ assets/js/data/swcache.js
@@ -0,0 +1,61 @@
+const resource = [
+
+  /* --- CSS --- */
+  '/gracee.github.io/assets/css/style.css',
+
+  /* --- PWA --- */
+  '/gracee.github.io/app.js',
+  '/gracee.github.io/sw.js',
+
+  /* --- HTML --- */
+  '/gracee.github.io/index.html',
+  '/gracee.github.io/404.html',
+  
+    '/gracee.github.io/categories/',
+  
+    '/gracee.github.io/tags/',
+  
+    '/gracee.github.io/archives/',
+  
+    '/gracee.github.io/about/',
+  
+
+  /* --- Favicons & compressed JS --- */
+  
+  
+    '/gracee.github.io/assets/img/favicons/android-chrome-192x192.png',
+    '/gracee.github.io/assets/img/favicons/android-chrome-512x512.png',
+    '/gracee.github.io/assets/img/favicons/apple-touch-icon.png',
+    '/gracee.github.io/assets/img/favicons/favicon-16x16.png',
+    '/gracee.github.io/assets/img/favicons/favicon-32x32.png',
+    '/gracee.github.io/assets/img/favicons/favicon.ico',
+    '/gracee.github.io/assets/img/favicons/mstile-150x150.png',
+    '/gracee.github.io/assets/js/dist/categories.min.js',
+    '/gracee.github.io/assets/js/dist/commons.min.js',
+    '/gracee.github.io/assets/js/dist/home.min.js',
+    '/gracee.github.io/assets/js/dist/misc.min.js',
+    '/gracee.github.io/assets/js/dist/page.min.js',
+    '/gracee.github.io/assets/js/dist/post.min.js',
+    '/gracee.github.io/assets/js/dist/pvreport.min.js'
+
+];
+
+/* The request url with below domain will be cached */
+const allowedDomains = [
+  
+
+  'Humanconsumerr.github.io',
+
+  
+
+  'fonts.gstatic.com',
+  'fonts.googleapis.com',
+  'cdn.jsdelivr.net',
+  'polyfill.io'
+];
+
+/* Requests that include the following path will be banned */
+const denyUrls = [
+  
+];
+
